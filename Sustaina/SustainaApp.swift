204c35924773187a3b5bb9ee72e41288d9b5585f
Noga Gercsak <nogah0317@gmail.com>
2025-01-22 18:45:47 -0500

    Initial Commit

---
+++ Sustaina/SustainaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainaApp.swift
+//  Sustaina
+//
+//  Created by Noga Gercsak on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SustainaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SustainaTests/SustainaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SustainaTests.swift
+//  SustainaTests
+//
+//  Created by Noga Gercsak on 1/22/25.
+//
+
+import Testing
+@testable import Sustaina
+
+struct SustainaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
